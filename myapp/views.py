aa8b1abf9dbeeb677adb8350431e8bc705100de3
NachiketaSingh13 <nachiketa13pakhi@gmail.com>
2025-05-04 15:22:03 +0530

    calorie counter

---
+++ myapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .models import Food, Consume
+
+def index(request):
+
+    if request.method == "POST":
+        food_consumed = request.POST['food_consumed']
+        consume = Food.objects.get(name=food_consumed)
+        user = request.user
+        consume = Consume(user=user, food_consumed=consume)
+        consume.save()
+        food = Food.objects.all()
+
+
+    else:
+        food = Food.objects.all()
+    consumed_food = Consume.objects.filter(user=request.user)
+    return render(request, 'myapp/index.html', {'food':food, 'consumed_food':consumed_food})
+
+
+def delete_consume(request, id):
+    consumed_food = Consume.objects.get(id=id)
+    if request.method == 'POST':
+        consumed_food.delete()
+        return redirect('/')
+    return render(request, 'myapp/delete.html')
+
